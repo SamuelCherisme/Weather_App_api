60727eca1f09f6a5f99d488e9e3fb102152c8913
talltrl <ssscherisme@gmail.com>
2020-09-24 16:42:27 -0400

    Heroku app

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ export default function App() {
   });
   const getWeather = async () => {
     try {
-      const response = await fetch('http://localhost:3000/weathers');
+      const response = await fetch('https://weather-ape.herokuapp.com/weathers');
       const data = await response.json();
       setWeather(data)
     } catch (error) {
